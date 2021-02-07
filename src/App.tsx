c92ef84b42af819ccb823df1aa3ea3446c5b5391
Brendan Molloy <brendan@bbqsrc.net>
2021-02-07 13:56:30 +0100

    Add subscription example

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,81 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+
+import { Client, defaultExchanges, Provider, subscriptionExchange, useQuery, useSubscription } from 'urql'
+import { SubscriptionClient } from 'subscriptions-transport-ws'
+
+const TASKRUNNER_API_HOST = "localhost:3001"
+
+const subscriptionClient = new SubscriptionClient(`ws://${TASKRUNNER_API_HOST}/graphql`, { reconnect: true });
+
+// https://formidable.com/open-source/urql/docs/advanced/subscriptions/#setting-up-subscriptions-transport-ws
+const client = new Client({
+  url: `//${TASKRUNNER_API_HOST}/graphql`,
+  exchanges: [
+    ...defaultExchanges,
+    subscriptionExchange({
+      forwardSubscription(operation) {
+        return subscriptionClient.request(operation)
+      },
+    }),
+  ],
+});
+
+const watchUpdatesSubscription = `
+  subscription {
+    watchUpdates
+  }
+`
+
+const someQuery = `
+  query {
+    hello
+  }
+`
+
+function TaskrunnerExample() {
+  // https://formidable.com/open-source/urql/docs/basics/queries/#run-a-first-query
+  const [result, reexecuteQuery] = useQuery({ query: someQuery })
+  const { data, fetching, error } = result
+
+  // https://formidable.com/open-source/urql/docs/advanced/subscriptions/#react--preact
+  const [res] = useSubscription({ query: watchUpdatesSubscription }, (data = [], evt) => {
+    // reexecuteQuery here causes `someQuery` to be re-run
+    reexecuteQuery({ requestPolicy: 'network-only' })
+
+    return [evt.watchUpdates, ...data]
+  })
+
+  if (res.error) {
+    return <p>{res.error}</p>
+  }
+  if (error) {
+    return <p>{error}</p>
+  }
+
+  if (!res.data) {
+    return <p>No data yet.</p>
+  }
+
+  return (
+    <>
+      <div>New data: {data.hello}</div>
+      <ul>
+        {res.data.map((evt: string, i: number) => (
+          <li key={i}>
+            {evt}
+          </li>
+        ))}
+      </ul>
+    </>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Provider value={client}>
+      <TaskrunnerExample />
+    </Provider>
+  )
 }
 
-export default App;
+export default App
